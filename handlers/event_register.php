753e6db69c134fd602fa6a3c67e414386e7de160
username <username@example.com>
2024-07-25 02:54:00 +0900

    trail phase

--- handlers/event_register.php
+++ handlers/event_register.php
@@ -9,8 +9,8 @@ $event_location = $_POST["event_loaction"];
 function print_name(...$form_data){
 	foreach($form_data as $key => $value){
 		echo $value."<br>";
-        }
+   }
 }
 
 print_name($event_name,$event_description, $start_date, $end_date, $event_location);
-?
+?>
